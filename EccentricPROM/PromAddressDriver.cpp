e61ebdc86be060394e9bb8fe568670d99cb48343
Benjamin <137060094+TheEccentricGenius@users.noreply.github.com>
2023-08-25 21:23:21 +1000

    Update PromAddressDriver.cpp

--- EccentricPROM/PromAddressDriver.cpp
+++ EccentricPROM/PromAddressDriver.cpp
@@ -1,6 +1,29 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2023 TheEccentricGenius
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 // This controls the shift register that generates the address lines for A0..A15 for most
-// chip families.  This is not used by the PromDevice8755A code.
-/* NOTE: Intel 8755A isn't supported.... */
+// chip families.
 // Note that this uses direct port control instead of digitalWrite calls so that the code
 // can run fast enough to meet the tBLC requirements for SDP and block writes.  This
 // sacrifices portability and readability for speed. //
